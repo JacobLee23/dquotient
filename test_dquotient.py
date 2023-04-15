5fdd438364a3f109248b5d0659f5428a01a95481
Jacob Lee <JLpython@outlook.com>
2023-04-15 14:09:46 -0700

    test: define tests for dquotient.DifferenceQuotient

--- test_dquotient.py
+++ test_dquotient.py
@@ -5,20 +5,37 @@ Unit tests for :py:mod:`dquotient`.
 from decimal import Decimal
 import pytest
 
-from dquotient import FiniteDifference
+from dquotient import DifferenceQuotient as DQuot
+from dquotient import FiniteDifference as FDiff
 from dquotient import FunctionRV
 
 
 @pytest.mark.parametrize(
     ("func", "x", "h", "fdiffs"),
     [
-        (lambda _: 0, 0, 0, FiniteDifference.FDifferences(0, 0, 0)),
+        (lambda _: 0, 0, 0, FDiff.FDifferences(0, 0, 0)),
     ]
 )
 def test_finitedifference(
-    func: FunctionRV, x: Decimal, h: Decimal, fdiffs: FiniteDifference.FDifferences
-    ):
+    func: FunctionRV, x: Decimal, h: Decimal, fdiffs: FDiff.FDifferences
+):
     """
     Unit tests for :py:class:`dquotient.FiniteDifference`.
     """
-    assert FiniteDifference.fdifferences(func, x, h) == fdiffs
+    assert FDiff.fdifferences(func, x, h) == fdiffs
+
+
+@pytest.mark.parametrize(
+    ("func", "x", "nderiv"),
+    [
+        (lambda _: 0, 0, DQuot.NDeriv(0, 0, 0)),
+    ]
+)
+def test_differencequotient(
+    func: FunctionRV, x: Decimal, nderiv: DQuot.NDeriv
+):
+    """
+    Unit tests for :py:class:`dquotient.DifferenceQuotient`.
+    """
+    diff = DQuot(func)
+    assert diff.nderiv(x) == nderiv
